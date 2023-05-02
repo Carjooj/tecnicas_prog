a5f27ba4b250e3c08e8510b194afbc7441811c5e
Carjooj <victor@4info.com.br>
2023-05-02 10:42:07 -0300

    Finalização dos métodos dos Vetores
    Método remover busca1, adicionaInicio, aumentaCapacidade e remove

--- TestaVetor/src/TestaVetor.java
+++ TestaVetor/src/TestaVetor.java
@@ -1,10 +1,22 @@
+import javax.swing.*;
 public class TestaVetor {
     public static void main(String[] args) throws Exception {
         Vetor vetor = new Vetor(5);
 
+        int indice;
+
         try {
+            vetor.adicionar("aff0");
             vetor.adicionar("aff1");
             vetor.adicionar("aff2");
+            vetor.adicionar("aff3");
+            vetor.adicionar("aff4");
+            vetor.adicionar("aff5");
+            vetor.adicionar("aff6");
+            vetor.adicionar("aff7");
+            vetor.adicionar("aff8");
+            vetor.adicionar("aff9");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -13,10 +25,22 @@ public class TestaVetor {
 
         System.out.println(vetor.toString());
 
+
         System.out.println(vetor.busca(0));
         System.out.println(vetor.busca(1));
 
-        System.out.println(vetor.busca(3));
+        System.out.println(vetor.busca1("Aff1"));
+        System.out.println(vetor.busca1("Aff2"));
+
+        int j = vetor.tamanho();
+        for (int i = 0; i < j; i++) {
+            vetor.remove(0);
+        }
+
+        char l = 65;
+        System.out.println(l);
+        System.out.println(vetor.toString());
+
 
 
 
--- TestaVetor/src/Vetor.java
+++ TestaVetor/src/Vetor.java
@@ -10,6 +10,7 @@ public class Vetor {
     }
 
     public void adicionar(String elemento) throws Exception {
+        this.aumentaCapacidade();
         if (this.tamanho < this.elementos.length) {
             this.elementos[this.tamanho] = elemento;
             this.tamanho++;
@@ -46,8 +47,52 @@ public class Vetor {
     }
 
     public String busca(int posicao) throws Exception {
+        if (posicao >= 0 && posicao < this.tamanho) {
+            return this.elementos[posicao];
+        }
+        else {
+            throw new Exception("Posição Inválida");
+        }
+    }
+
+    public int busca1(String elemento) {
+        for (int i = 0; i < this.tamanho; i++) {
+            if (this.elementos[i].equalsIgnoreCase(elemento)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean adcionaInicio(int posicao, String elemento) throws Exception {
+        this.aumentaCapacidade();
         if (posicao >= 0 && posicao < tamanho) {
-            return elementos[posicao];
+            for (int i = this.tamanho; i > posicao - 1; i--) {
+                this.elementos[i + 1] = this.elementos[i];
+            }
+            this.elementos[posicao] = elemento;
+            this.tamanho++;
+        }
+        else {
+            throw new Exception("Posicão Inválida");
+        }
+        return true;
+    }
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            String[] elementosNovos = new String[this.elementos.length * 2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+    }
+    public void remove(int posicao) throws Exception {
+        if (posicao >= 0 && posicao < tamanho) {
+            for (int i = posicao; i < this.tamanho - 1; i++) {
+                this.elementos[i] = this.elementos[i + 1];
+            }
+            this.tamanho--;
         }
         else {
             throw new Exception("Posição Inválida");
